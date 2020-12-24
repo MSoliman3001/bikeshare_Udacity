cb70eb9148fdc4fb1ffc9aee91889553c36c9f0d
MSoliman3001 <76523194+MSoliman3001@users.noreply.github.com>
2020-12-24 12:38:27 +0200

    Update Bikeshare.py

--- Bikeshare.py
+++ Bikeshare.py
@@ -8,7 +8,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def input_Check (input_str,input_type):
     while True:
-        input_read = input(input_str)
+        input_read = input(input_str).lower()
         try:
             if input_read in ['all','chicago','new york city','washington'] and input_type == 1:
                 break
@@ -44,7 +44,7 @@ def load_data(city, month, day):
     # filter by month if applicable
     if month != 'all':
         months = ['january', 'february', 'march', 'april', 'may', 'june']
-        month = months.index(month) + 1
+        month = months.index(month.lower()) + 1
         df = df[df['month'] == month]
     # filter by day of week if applicable
     if day != 'all':
@@ -79,17 +79,27 @@ def station_stats(df):
 def trip_duration_stats(df):
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
-    total_travel=df['Trip Duration'].sum()
-    print('total travel time is: \n',total_travel)
+	
+    total_travel = df['Trip Duration'].sum()
+
+    total_by_hours = total_travel / (60*60)
+    total_travel_days = total_by_hours // 24
+    total_travel_hours = total_by_hours % 24
+    print('total travel time is: \n {} days \n {} hours ').format(total_travel_days,total_travel_hours)
+    
     mean_travel = df['Trip Duration'].mean()
-    print('total travel time is: \n',total_travel)
+    mean_by_hours = mean_travel / (60*60)
+    mean_travel_days = mean_by_hours // 24
+    mean_travel_hours = mean_by_hours % 24
+    print('total travel time is: \n {} days \n {} hours').format(mean_travel_days, mean_travel_hours)
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 def user_stats(df,city):
     print('\nCalculating User Stats...\n')
     start_time = time.time()
-    usr_typ = df['User Types'].value_counts()
+    usr_typ = df['User Type'].value_counts()
     print('counts of user types is: \n',usr_typ)
     if city != "washington":
         print('- counts of Gender :')
@@ -104,6 +114,20 @@ def user_stats(df,city):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
+	
+df display_data (df):
+    print('\nCalculating display data...\n')
+    	start_time = time.time()
+	view_data = input('\nWould you like to view 5 rows of individual trip data? Enter yes or no\n')
+	if view_data == "yes":
+	    start_loc = 0
+	    while True:
+    		print(df.iloc[start_loc:start_loc+5])
+		start_loc += 5
+    		view_display = input("Do you wish to continue with next 5 rows?: Enter yes or no.\n “).lower()
+		if restart.lower() != "yes":
+			break
+	
 def main():
     while True:
         city, month, day = get_filters()
@@ -112,9 +136,10 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df,city)
+	display_data (df)
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
-            break
+	    break
 
 
 if __name__ == "__main__":
